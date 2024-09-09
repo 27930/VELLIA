6968d11afdfaa1acd4ab11ac7be109a2aab11ad4
mustafa56re <mustafaabojmail577@gmail.com>
2024-09-09 09:49:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+function toggleMenu() {
+    var menu = document.querySelector('.dropdown-menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+</script>
